c0af5fe6afef4427eacd86b4be5f6831596a95bd
Nikos Kefalas <nikoskef@outlook.com>
2022-01-21 09:30:46 +0200

    breadcrumbs

--- src/app/pages/details/details.page.ts
+++ src/app/pages/details/details.page.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Transaction } from '../../services/cash.service';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.page.html',
-  styleUrls: ['./details.page.scss'],
+  styleUrls: ['./details.page.scss']
 })
 export class DetailsPage implements OnInit {
+  transaction: Transaction = null;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private route: ActivatedRoute, private router: Router) {
+    this.route.queryParams.subscribe(() => {
+      if (this.router.getCurrentNavigation().extras.state) {
+        this.transaction = this.router.getCurrentNavigation().extras.state.transaction;
+      }
+    });
   }
 
+  ngOnInit() {}
 }
